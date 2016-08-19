8bd03f3e433da0ec642fcb87b89503abcf5345b8
Anton Katunin <antulik@gmail.com>
2016-08-19 22:18:41 +1000

    removed unused route helper as it's too complicated to use

--- lib/pagelet_rails/concerns/routes.rb
+++ lib/pagelet_rails/concerns/routes.rb
@@ -29,43 +29,6 @@ module PageletRails::Concerns::Routes
       end
     end
 
-    # Define inline single route for the following method.
-    # It automatically adds :controller and :action names to the route
-    #
-    #     class PageletRails::Examples::ExamplesController
-    #       pageletlet_route :get, ''
-    #       def bingo
-    #       end
-    #     end
-    #
-    # will generate routes
-    #   Helper:            pagelets_examples_path
-    #   HTTP Verb:         GET
-    #   Path:              /pagelets/examples(.:format)
-    #   Controller#Action: pagelets/examples/examples#bingo
-    #
-    def pagelet_route *args
-      @pagelet_route << args
-    end
-
-    def method_added method_name
-      return unless @pagelet_route
-      @pagelet_route.each do |args|
-        options = args.extract_options!
-        options[:controller] ||= self.controller_name
-        options[:action] ||= method_name
-
-        @pagelet_routes << Proc.new do
-          scope path: options[:controller], as: options[:controller] do
-            self.send *args, options
-          end
-        end
-      end
-
-      @pagelet_route = []
-      super
-    end
-
     def load_pagelet_routes! context
       @pagelet_routes.each do |proc|
         context.instance_eval &proc
@@ -74,7 +37,6 @@ module PageletRails::Concerns::Routes
 
     def inherited subklass
       subklass.instance_variable_set(:@pagelet_routes, [])
-      subklass.instance_variable_set(:@pagelet_route, [])
       super
     end
   end
